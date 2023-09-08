3cda7df65b2b558e2af4c514f8c3042670e9e661
kChmiel <chmieluska@gmail.com>
2023-09-08 14:10:16 +0200

    [TASK9] Person.java updated 5th time

--- Person.java
+++ Person.java
@@ -1,28 +1,6 @@
 public class Person {
     public static void main(String[] args) {
         System.out.println("Hello1");
-        System.out.println("Hello2");
-        System.out.println("Hello3");
-        System.out.println("Hello4");
-        System.out.println("Hello6");
-        System.out.println("Hello6");
-        System.out.println("Hello6");
-        System.out.println("Hello6");
 
-        System.out.println("Hello5");
-        System.out.println("Hello5");
-        System.out.println("Hello5");
-        System.out.println("Hello9");
-
-
-        System.out.println("Hello5");
-        System.out.println("Hello5");
-        System.out.println("Hello5");
-        System.out.println("Hello5");
-
-        System.out.println("Hello8");
-        System.out.println("Hello8");
-        System.out.println("Hello8");
-        System.out.println("Hello8");
     }
 }
